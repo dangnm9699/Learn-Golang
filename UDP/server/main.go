b8c1089ec58d7dcea1e09f38e092d93a2cd4441e
huongtt51 <huongtt51@vttek.vn>
2020-07-31 10:04:17 +0700

    edited

--- UDP/server/main.go
+++ UDP/server/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func main() {
@@ -20,11 +23,36 @@ func main() {
 		log.Fatal(err)
 	}
 	defer conn.Close()
-	go handleConn(conn)
+	buf := make([]byte, 1024)
+	cls()
+	for {
+		nbytes, rAddr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		receive := string(buf[:nbytes])
+		// If quit
+		if strings.Split(strings.TrimSpace(receive), " ")[0] == "QUIT" {
+			fmt.Printf("%s -> Disconnected\n", rAddr.String())
+			continue
+		}
+		// Print message
+		fmt.Printf("%s -> %s", rAddr.String(), receive)
+		go handleConn(conn, rAddr)
+	}
 }
 
-func handleConn(c *net.UDPConn) {
-	for {
-		
+func handleConn(c *net.UDPConn, rAddr *net.UDPAddr) {
+	_, err := c.WriteToUDP([]byte("200 OK\n"), rAddr)
+	if err != nil {
+		fmt.Println("Some errors occurred while sending response")
 	}
+}
+
+// Clear Screen
+func cls() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
