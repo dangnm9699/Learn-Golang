99e621844a70ca0bf3ee6dcaeaf97366fef78fd1
Nguyễn Minh Đăng <meucute99@gmail.com>
2020-08-02 02:21:09 +0700

    update file

--- Knight Tour/main.go
+++ Knight Tour/main.go
@@ -23,21 +23,11 @@ func main() {
 	}
 	board[r][c] = 1
 	step++
-	// cls()                                           // OPTIONAL
-	// go spinner()                                    // OPTIONAL
-	KnightTour(r, c) // NECESSARY
-	// cls()                                           // OPTIONAL
-	// fmt.Printf("There are %d solutions", solutions) // OPTIONAL
+	knightTour(r, c)
 }
 
-// KnightTour is a function that check if a bishop
-// an go to every places of chess board
-func KnightTour(r, c int) {
+func knightTour(r, c int) {
 	if step == size*size {
-		// ALTERNATIVE
-		// solutions++
-		// return
-		// REQUIRED
 		for i := 1; i <= size; i++ {
 			for j := 1; j <= size; j++ {
 				fmt.Printf("%d ", board[i][j])
@@ -54,38 +44,10 @@ func KnightTour(r, c int) {
 			if board[nextRow][nextCol] == 0 {
 				step++
 				board[nextRow][nextCol] = step
-				KnightTour(nextRow, nextCol)
+				knightTour(nextRow, nextCol)
 				board[nextRow][nextCol] = 0
 				step--
 			}
 		}
 	}
 }
-
-// func printSolution() {
-// 	fmt.Printf("Solution: %d\n", solutions)
-// 	fmt.Println(strings.Repeat("-", 3*size-1))
-// 	for i := 1; i <= size; i++ {
-// 		for j := 1; j <= size; j++ {
-// 			fmt.Printf("%-3d", board[i][j])
-// 		}
-// 		fmt.Print("\n")
-// 	}
-// 	fmt.Println(strings.Repeat("-", 3*size-1))
-// 	fmt.Print("\n\n")
-// }
-
-// func spinner() {
-// 	for {
-// 		for _, c := range `-\|/` {
-// 			fmt.Printf("\r%c", c)
-// 			time.Sleep(100 * time.Millisecond)
-// 		}
-// 	}
-// }
-
-// func cls() {
-// 	cmd := exec.Command("cmd", "/c", "cls")
-// 	cmd.Stdout = os.Stdout
-// 	cmd.Run()
-// }
