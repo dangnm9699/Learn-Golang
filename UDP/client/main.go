d6bb07e8ed72d432c73fa8005c65abc06a44bd26
huongtt51 <huongtt51@vttek.vn>
2020-08-07 17:55:41 +0700

    upload new files

--- UDP/client/main.go
+++ UDP/client/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+)
 
+func main() {
+	rAddr, err := net.ResolveUDPAddr("udp4", ":8000")
+	checkErr(err)
+	conn, err := net.DialUDP("udp4", nil, rAddr)
+	checkErr(err)
+	for {
+		sendData(conn)
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func sendData(c *net.UDPConn) {
+	// send := Packet{
+	// 	Cmd: rand.Int31n(2) + 1,
+	// 	Info: &User{
+	// 		Id:    os.Args[1],
+	// 		Score: rand.Int31n(10),
+	// 	},
+	// }
+	// data, err := proto.Marshal(&send)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	c.Write([]byte("HELLO"))
+	//time.Sleep(time.Second)
 }
--- UDP/server/main.go
+++ UDP/server/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
+func main() {
+	addr, err := net.ResolveUDPAddr("udp4", ":8000")
+	checkErr(err)
+	conn, err := net.ListenUDP("udp4", addr)
+	checkErr(err)
+	for {
+		handler(conn)
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(c *net.UDPConn) {
+	buf := make([]byte, 2048)
+	nbytes, addr, err := c.ReadFromUDP(buf)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	data := buf[:nbytes]
+	// var receive Packet
+	// if err := proto.Unmarshal(data, &receive); err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
+	// fmt.Println(addr.String(), receive.Cmd, receive.Info.Id, receive.Info.Score)
+	fmt.Println(addr.String(), data)
 }
