55029a2fc067816a081a842ccfe71530b083a7bb
Nguyễn Minh Đăng <meucute99@gmail.com>
2020-08-08 16:16:29 +0700

    update files

--- UDP/client/main.go
+++ UDP/client/main.go
@@ -1,38 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
 	"net"
+	"os"
+
+	"google.golang.org/protobuf/proto"
 )
 
 func main() {
-	rAddr, err := net.ResolveUDPAddr("udp4", ":8000")
-	checkErr(err)
-	conn, err := net.DialUDP("udp4", nil, rAddr)
-	checkErr(err)
-	for {
-		sendData(conn)
-	}
-}
-
-func checkErr(err error) {
+	rAddr, err := net.ResolveUDPAddr("udp4", "localhost:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
+	conn, err := net.DialUDP("udp4", nil, rAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Goroutine here
+	go func() {
+		for {
+			sendData(conn)
+		}
+	}()
+	for {
+		readResp(conn)
+	}
+}
+func sendData(c *net.UDPConn) {
+	send := Packet{
+		Cmd: rand.Int31n(2) + 1,
+		Info: &User{
+			Id:    os.Args[1],
+			Score: rand.Int31n(10),
+		},
+	}
+	data, err := proto.Marshal(&send)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.Write(data)
+	// time.Sleep(time.Second)
 }
 
-func sendData(c *net.UDPConn) {
-	// send := Packet{
-	// 	Cmd: rand.Int31n(2) + 1,
-	// 	Info: &User{
-	// 		Id:    os.Args[1],
-	// 		Score: rand.Int31n(10),
-	// 	},
-	// }
-	// data, err := proto.Marshal(&send)
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	c.Write([]byte("HELLO"))
-	//time.Sleep(time.Second)
+func readResp(c *net.UDPConn) {
+	buf := make([]byte, 2048)
+	nbytes, _, err := c.ReadFromUDP(buf)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Println(string(buf[:nbytes]))
 }
--- UDP/server/main.go
+++ UDP/server/main.go
@@ -4,37 +4,41 @@ import (
 	"fmt"
 	"log"
 	"net"
+
+	"google.golang.org/protobuf/proto"
 )
 
 func main() {
-	addr, err := net.ResolveUDPAddr("udp4", ":8000")
-	checkErr(err)
-	conn, err := net.ListenUDP("udp4", addr)
-	checkErr(err)
-	for {
-		handler(conn)
-	}
-}
-
-func checkErr(err error) {
+	addr, err := net.ResolveUDPAddr("udp4", "localhost:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
+	conn, err := net.ListenUDP("udp4", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		handleConn(conn)
+	}
 }
 
-func handler(c *net.UDPConn) {
+func handleConn(c *net.UDPConn) {
 	buf := make([]byte, 2048)
 	nbytes, addr, err := c.ReadFromUDP(buf)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	data := buf[:nbytes]
-	// var receive Packet
-	// if err := proto.Unmarshal(data, &receive); err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
-	// fmt.Println(addr.String(), receive.Cmd, receive.Info.Id, receive.Info.Score)
-	fmt.Println(addr.String(), data)
+	// Goroutine here to execute
+	go execute(c, addr, buf[:nbytes])
+}
+
+func execute(c *net.UDPConn, addr *net.UDPAddr, data []byte) {
+	var receive Packet
+	if err := proto.Unmarshal(data, &receive); err != nil {
+		log.Println(err)
+		return
+	}
+	c.WriteToUDP([]byte(addr.String()+" 200 OK"), addr)
+	fmt.Println(addr.String(), receive.Cmd)
 }
